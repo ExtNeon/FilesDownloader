e7926c38aadb52fc1b99c52ea37173ca1f71c0a9
Кирилл Малякин <malyakin-10@yandex.ru>
2017-12-13 21:20:14 +0300

    Контроль версий в идее сошёл с ума. Попытка всё восстановить

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,11 @@ public class Main {
     private static ArrayList<String> musicLinks = new ArrayList<>();
 
     public static void main(String[] args) {
-            getMusicLinks();
-            downloadMusic();
-            System.out.println("Complete.");
+        getMusicLinks();
+        downloadMusic();
+        System.out.println("Complete.");
     }
 
-    /**
-     * Попытка вс
-     */
     private static void getMusicLinks() {
         try (BufferedWriter musicListFile = new BufferedWriter(new FileWriter(MUSIC_LIST_FILE))) {
             ArrayList<String> URLFromFile = readAllLinksInFile(URL_FILE);
@@ -40,23 +37,32 @@ public class Main {
     private static void downloadMusic() {
         ArrayList<URLDownloader> downloaders = createDownloaders();
         delay(1000);
+        long lastMsCount = System.currentTimeMillis();
         waitWhileAllSongsWillBeDownloaded(downloaders);
-        System.out.println("\n" + musicLinks.size() + " files was downloaded successful.\n");
+        System.out.println("\n" + musicLinks.size() + " files was downloaded successful.\n" +
+                "It spend: " + formatSecondsToNormalTime((int)((System.currentTimeMillis() - lastMsCount) / 1000)));
     }
 
     private static void waitWhileAllSongsWillBeDownloaded(ArrayList<URLDownloader> downloaders) {
-        for (int i = 0; downloaders.size() > 0; i++) {
-            if (i > downloaders.size()) {
+        int downloadedPercent, i = 0;
+        while (downloaders.size() > 0) {
+            if (i >= downloaders.size()) {
                 i = 0;
-            }
-            if (downloaders.get(i).isDownloaded()) {
-                System.out.print("\r" + downloaders.get(i).getFilename() + " - done");
-                downloaders.remove(i);
-                i = 0;
+            } else {
+                if (downloaders.get(i).isDownloaded()) {
+                    downloadedPercent = 100 - (((downloaders.size() - 1) * 100) / musicLinks.size());
+                    System.out.print("\r" + downloaders.get(i).getFilename() + " - done\n" + getPercentLine(downloadedPercent) + " done");
+                    downloaders.remove(i);
+                    i = 0;
+                } else i++;
             }
         }
     }
 
+    /**
+     * Создаёт и возвращает загрузчики. ь
+     * @return
+     */
     private static ArrayList<URLDownloader> createDownloaders() {
         ArrayList<URLDownloader> downloaders = new ArrayList<>(musicLinks.size());
         for (int count = 0; count < musicLinks.size(); count++) {
@@ -67,6 +73,28 @@ public class Main {
         return downloaders;
     }
 
+    private static String getPercentLine(int percents) {
+        StringBuilder temp = new StringBuilder("[");
+        percents /= 2;
+        for (int i = 1; i <= 50; i++) {
+            if (i <= percents) {
+                temp.append('=');
+            } else {
+                temp.append(' ');
+            }
+        }
+        temp.append("] | ");
+        temp.append(percents * 2);
+        temp.append('%');
+        return temp.toString();
+    }
+
+    private static String formatSecondsToNormalTime(int seconds) {
+        int s = seconds % 60;
+        int m = (seconds / 60) % 60;
+        int h = (seconds / (60 * 60)) % 24;
+        return String.format("%d:%02d:%02d", h, m, s);
+    }
 
     private static String getPageCode(String inUrl) {
         try (BufferedReader pageReader = new BufferedReader(new InputStreamReader(new URL(inUrl).openStream()))) {
